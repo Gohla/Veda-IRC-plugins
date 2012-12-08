f5bb0a2552cb265300fdb583f7e9fc0b78a6c1ca
Gabriël Konat <gabrielkonat@gmail.com>
2012-12-08 23:12:59 +0100

    Added Help and Plugin plugin.

---
+++ Help/HelpPlugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veda.Command;
+using Veda.Plugin;
+using Gohla.Shared;
+
+namespace Veda.Plugins.Help
+{
+    public class HelpPlugin : IPlugin
+    {
+        private List<ICommand> _commands = new List<ICommand>();
+
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public IEnumerable<ICommand> Commands { get { return _commands; } }
+        public String Key { get { return Name; } }
+
+        public HelpPlugin()
+        {
+            Name = "Help";
+            Description = "Provides help for plugins and commands.";
+
+            _commands.Add(CommandBuilder.CreateCommand<Func<IContext, IPlugin, String>>
+            (
+                "help", 
+                "Shows help about a plugin.", 
+                (context, plugin) => Help(context, plugin))
+            );
+            _commands.Add(CommandBuilder.CreateCommand<Func<IContext, IPlugin, String, String>>
+            (
+                "help",
+                "Shows help about a command in a plugin.", 
+                (context, plugin, command) => Help(context, plugin, command))
+            );
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public String Help(IContext context, IPlugin plugin)
+        {
+            return plugin.Description;
+        }
+
+        public String Help(IContext context, IPlugin plugin, String command)
+        {
+            IEnumerable<ICommand> candidates = plugin.Commands
+                .Where(c => c.Name.Equals(command))
+                ;
+
+            if(candidates.IsEmpty())
+                return "Command " + command + " not found in " + plugin.Name + ".";
+
+            return candidates
+                .Select(c => "(" + c.Name + " " + c.ParameterTypes.Skip(1).ToString(", ") + "): " + c.Description)
+                .ToString(" | ")
+                ;
+        }
+    }
+}
---
+++ Plugin/PluginPlugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Veda.Command;
+using Veda.Plugin;
+
+namespace Veda.Plugins.Plugin
+{
+    public class PluginPlugin : IPlugin
+    {
+        private List<ICommand> _commands = new List<ICommand>();
+
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public IEnumerable<ICommand> Commands { get { return _commands; } }
+        public String Key { get { return Name; } }
+
+        public PluginPlugin()
+        {
+            Name = "Plugin";
+            Description = "Manages and retrieves information about plugins.";
+
+            _commands.Add(CommandBuilder.CreateCommand<Func<IContext, String>>
+            (
+                "list", 
+                "Lists all plugins.",
+                context => List(context))
+            );
+            _commands.Add(CommandBuilder.CreateCommand<Func<IContext, IPlugin, String>>
+            (
+                "list",
+                "Lists all commands in a plugin.",
+                (context, plugin) => List(context, plugin))
+            );
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public String List(IContext context)
+        {
+            return context.Bot.PluginManager.Plugins
+                .Select(p => p.Name)
+                .ToString(" ")
+                ;
+        }
+
+        public String List(IContext context, IPlugin plugin)
+        {
+            return plugin.Commands
+                .Select(p => p.Name)
+                .Distinct()
+                .ToString(" ")
+                ;
+        }
+    }
+}
