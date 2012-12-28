1f64c7c7e122e7bd5c59e4be18c716ebb43c9016
Gabriël Konat <gabrielkonat@gmail.com>
2012-12-28 23:41:04 +0100

    Added commands to retrieve and match identities. Added commands to add, remove and list user identity masks.

--- User/UserPlugin.cs
+++ User/UserPlugin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using ReactiveIRC.Interface;
 using Veda.Interface;
+using System.Linq;
 
 namespace Veda.Plugins.User
 {
@@ -19,36 +21,90 @@ namespace Veda.Plugins.User
             context.Bot.Authentication.Identify(context.Sender, username, password);
         }
 
-        [Command(Description = "Returns your identified username.")]
-        public static String Username(IContext context)
+        [Command(Description = "Retrieves your identity.")]
+        public static IIdentity Identity(IContext context)
         {
-            if(!context.Bot.Authentication.IsIdentified(context.Sender))
-                throw new InvalidOperationException("You are not identified.");
-            return context.User.Username;
+            return context.Sender.Identity;
+        }
+
+        [Command(Description = "Retrieves the identity of given user.")]
+        public static IIdentity Identity(IContext context, IUser user)
+        {
+            return user.Identity;
+        }
+
+        [Command(Description = "Tries to match given identity to given identity mask.")]
+        public static bool IdentityMatch(IContext context, IIdentity identity, IdentityMask mask)
+        {
+            return mask.Match(identity);
+        }
+
+        [Command(Description = "Adds your current identity mask to your identity masks. Any commands invoked under that mask are automatically identified as you.")]
+        public static void AddMask(IContext context)
+        {
+            EnsureIdentified(context);
+            context.Bot.Authentication.AddMask(context.User, new IdentityMask(context.Sender.Identity));
+        }
+
+        [Command(Description = "Adds an identity mask to your identity masks. Any commands invoked under that mask are automatically identified as you.")]
+        public static void AddMask(IContext context, IdentityMask mask)
+        {
+            EnsureIdentified(context);
+            context.Bot.Authentication.AddMask(context.User, mask);
+        }
+
+        [Command(Description = "Remove a mask from your identity masks.")]
+        public static void RemoveMask(IContext context, IdentityMask mask)
+        {
+            EnsureIdentified(context);
+            context.Bot.Authentication.RemoveMask(context.User, mask);
+        }
+
+        [Command(Description = "Lists your identity masks.")]
+        public static IEnumerable<IdentityMask> ListMasks(IContext context)
+        {
+            EnsureIdentified(context);
+            return context.Bot.Authentication.Masks(context.User);
         }
 
         [Command(Description = "Returns your identified username.")]
+        public static String Username(IContext context)
+        {
+            EnsureIdentified(context);
+            return context.User.Username;
+        }
+
+        [Command(Description = "Returns the identified username for given user.")]
         public static String Username(IContext context, IUser user)
         {
-            if(!context.Bot.Authentication.IsIdentified(user))
-                throw new InvalidOperationException("User is not identified.");
+            EnsureIdentified(context, user);
             return context.Bot.Authentication.GetUser(user).Username;
         }
 
         [Command(Description = "Returns your identified group.")]
         public static String Group(IContext context)
         {
-            if(!context.Bot.Authentication.IsIdentified(context.Sender))
-                throw new InvalidOperationException("You are not identified.");
+            EnsureIdentified(context);
             return context.User.Group.Name;
         }
 
-        [Command(Description = "Returns your identified group.")]
+        [Command(Description = "Returns the identified group for given user.")]
         public static String Group(IContext context, IUser user)
+        {
+            EnsureIdentified(context, user);
+            return context.Bot.Authentication.GetUser(user).Group.Name;
+        }
+
+        private static void EnsureIdentified(IContext context, IUser user)
         {
             if(!context.Bot.Authentication.IsIdentified(user))
                 throw new InvalidOperationException("User is not identified.");
-            return context.Bot.Authentication.GetUser(user).Group.Name;
+        }
+
+        private static void EnsureIdentified(IContext context)
+        {
+            if(!context.Bot.Authentication.IsIdentified(context.Sender))
+                throw new InvalidOperationException("You are not identified. Use Identify to identify yourself with the bot, or use Register to register an account with the bot.");
         }
     }
 }
