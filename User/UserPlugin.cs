dc08c6c72dc6ef7fafaf5f8018fd452ea7533122
Gabriël Konat <gabrielkonat@gmail.com>
2013-01-03 16:24:12 +0100

    Added commands for unregister and unidentify.

--- User/UserPlugin.cs
+++ User/UserPlugin.cs
@@ -14,10 +14,23 @@ namespace Veda.Plugins.User
             context.Bot.Authentication.Register(context.Sender, username, password);
         }
 
+        [Command(Description = "Unregister your account. Username and password must be given for confirmation.", Private = true)]
+        public static void Unregister(IContext context, String username, String password)
+        {
+            EnsureIdentified(context);
+            context.Bot.Authentication.Unregister(context.Sender, context.User, username, password);
+        }
+
         [Command(Description = "Identify as user with given username and password.", Private = true)]
         public static void Identify(IContext context, String username, String password)
         {
             context.Bot.Authentication.Identify(context.Sender, username, password);
+        }
+
+        [Command(Description = "Unidentifies you from the bot.")]
+        public static void Unidentify(IContext context)
+        {
+            context.Bot.Authentication.Unidentify(context.Sender);
         }
 
         [Command(Description = "Adds your current identity mask to your identity masks. Any commands invoked under that mask are automatically identified as you.")]
