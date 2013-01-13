b24a5137ca4a39253eb407f8ed7beaa935a3da5c
Gabriël Konat <gabrielkonat@gmail.com>
2013-01-13 18:05:45 +0100

    Use multi word command names in list plugin as well.

--- List/ListPlugin.cs
+++ List/ListPlugin.cs
@@ -8,8 +8,8 @@ namespace Veda.Plugins.List
     [Plugin(Name = "List", Description = "Created, modifies and retrieves lists.")]
     public class ListPlugin
     {
-        [Command(Description = "Creates a global list with given identifier.")]
-        public void Create(IContext context, String identifier)
+        [Command(Name = "create global", Description = "Creates a global list with given identifier.")]
+        public void CreateGlobal(IContext context, String identifier)
         {
             IStorage storage = context.Bot.Storage.Global(context.Command.Plugin);
             List<object> list = storage.Get<List<object>>(identifier);
@@ -18,8 +18,8 @@ namespace Veda.Plugins.List
             storage.Set(new List<object>(), identifier);
         }
 
-        [Command(Description = "Creates a server-scoped list on given server with given identifier. Server parameter may be omitted to select the current server.")]
-        public void Create(IContext context, IClientConnection server, String identifier)
+        [Command(Name = "create server", Description = "Creates a server-scoped list on given server with given identifier.")]
+        public void CreateServer(IContext context, IClientConnection server, String identifier)
         {
             IStorage storage = context.Bot.Storage.Server(context.Command.Plugin, server);
             List<object> list = storage.Get<List<object>>(identifier);
@@ -28,13 +28,36 @@ namespace Veda.Plugins.List
             storage.Set(new List<object>(), identifier);
         }
 
-        [Command(Description = "Creates a channel-scoped list on given channel with given identifier. Channel parameter may be omitted to select the current channel.")]
-        public void Create(IContext context, IChannel channel, String identifier)
+        [Command(Name = "create server", Description = "Creates a server-scoped list on the current server with given identifier.")]
+        public void CreateServer(IContext context, String identifier)
+        {
+            IStorage storage = context.Bot.Storage.Server(context.Command.Plugin, context.Connection);
+            List<object> list = storage.Get<List<object>>(identifier);
+            if(list != null)
+                throw new ArgumentException("List with given identifier already exists for the current server.", "identifier");
+            storage.Set(new List<object>(), identifier);
+        }
+
+        [Command(Name = "create channel", Description = "Creates a channel-scoped list on given channel with given identifier.")]
+        public void CreateChannel(IContext context, IChannel channel, String identifier)
         {
             IStorage storage = context.Bot.Storage.Channel(context.Command.Plugin, channel);
             List<object> list = storage.Get<List<object>>(identifier);
             if(list != null)
                 throw new ArgumentException("List with given identifier already exists for given channel.", "identifier");
+            storage.Set(new List<object>(), identifier);
+        }
+
+        [Command(Name = "create channel", Description = "Creates a channel-scoped list on the current channel with given identifier.")]
+        public void CreateChannel(IContext context, String identifier)
+        {
+            if(context.Channel == null)
+                throw new InvalidOperationException("Command must be executed in a channel.");
+
+            IStorage storage = context.Bot.Storage.Channel(context.Command.Plugin, context.Channel);
+            List<object> list = storage.Get<List<object>>(identifier);
+            if(list != null)
+                throw new ArgumentException("List with given identifier already exists for the current channel.", "identifier");
             storage.Set(new List<object>(), identifier);
         }
 
