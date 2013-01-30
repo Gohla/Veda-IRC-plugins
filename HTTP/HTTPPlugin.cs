fad2d57de15a0007097d98806752ed0933a557cd
Gabriël Konat <gabrielkonat@gmail.com>
2013-01-30 20:16:50 +0100

    Added HTTP plugin.

---
+++ HTTP/HTTPPlugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using Gohla.Shared;
+using Veda.Interface;
+
+namespace Veda.Plugins.HTTP
+{
+    [Plugin(Name = "HTTP")]
+    public static class HTTPPlugin
+    {
+        private static ObservableStringWebRequest _webRequest = new ObservableStringWebRequest();
+        private static Regex _titleRegex = new Regex(@"<title>\s*(.+?)\s*</title>", RegexOptions.Compiled, 
+            TimeSpan.FromMilliseconds(100));
+
+        [Command(Description = "Performs a HTTP request for given URL and returns the contents of the response.")]
+        public static IObservable<String> Request(IContext context, String url)
+        {
+            return _webRequest.Request(url, x => x);
+        }
+
+        [Command(Description = "Performs a HTTP request for given URL and returns HTML title of the response.")]
+        public static IObservable<String> Title(IContext context, String url)
+        {
+            return _webRequest
+                .Request(url, x => x)
+                .Select(GetTitle)
+                ;
+        }
+
+        [Command(Description = "Encodes given text as a HTTP URL.")]
+        public static String URLEncode(IContext context, String text)
+        {
+            return HttpUtility.UrlEncode(text);
+        }
+
+        [Command(Description = "Decodes given HTTP URL to text.")]
+        public static String URLDecode(IContext context, String httpURL)
+        {
+            return HttpUtility.UrlDecode(httpURL);
+        }
+
+        private static String GetTitle(String html)
+        {
+            Match match = _titleRegex.Match(html);
+            if(match.Success)
+                return match.Groups[1].Value;
+            else
+                return String.Empty;
+        }
+    }
+}
---
+++ HTTP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Veda.Plugins.HTTP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Veda.Plugins.HTTP")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3af48547-aa8d-4418-8cc6-94ff00327d7e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
